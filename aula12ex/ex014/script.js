75261f2501d0cc304205f7759fe80e6c41592435
Wilson junior <wilsonjunior-pdm@hotmail.com>
2023-05-26 20:05:21 -0300

    Atualizando....

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var foto = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // Bom dia!
+        foto.src = 'fotomanha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        // Boa tarde!
+        foto.src = 'fototarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        // Boa noite!
+        foto.src = 'fotonoite.png'
+        document.body.style.background = '#515154'
+    }
+    
+}
