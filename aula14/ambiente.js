97adf8956872f366338161bc84e210f1d7861f63
Wilson junior <wilsonjunior-pdm@hotmail.com>
2023-06-02 20:17:24 -0300

    Create ambiente.js

---
+++ aula14/ambiente.js
@@ -0,0 +1,15 @@
+/*var c = 1 
+while (c <= 5) {
+    console.log(c)
+    c = c + 1
+}*/
+
+/*var c = 1 
+do {
+    console.log(c)
+    c = c + 1
+} while (c <= 5) */
+
+for (var c =1; c <= 10; c = c + 1) {
+    console.log(c)
+}
